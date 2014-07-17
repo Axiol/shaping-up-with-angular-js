ec2fe76e5a3ee7ea740e5ba31daaa7db69f26585
Arnaud Delante <ad7030@gmail.com>
2014-07-17 22:43:14 +0200

    End of level 4

--- js/app.js
+++ js/app.js
@@ -110,24 +110,38 @@
     this.products = gems;
   });
 
-  app.controller('PanelController', function() {
-    this.tab = 1;
-
-    this.selectTab = function(setTab) {
-      this.tab = setTab;
-    };
-
-    this.isSelected = function(checkTab) {
-      return this.tab === checkTab;
-    };
-  });
-
   app.controller('ReviewController', function() {
     this.review = {};
 
     this.addReview = function(product) {
       product.reviews.push(this.review);
       this.review = {};
+    };
+  });
+
+  app.directive('productTitle', function() {
+    return {
+      restrict: 'E',
+      templateUrl: './includes/gem-title.html'
+    };
+  });
+
+  app.directive('productPanels', function() {
+    return {
+      restrict: 'E',
+      templateUrl: './includes/gem-panels.html',
+      controller: function() {
+        this.tab = 1;
+
+        this.selectTab = function(setTab) {
+          this.tab = setTab;
+        };
+
+        this.isSelected = function(checkTab) {
+          return this.tab === checkTab;
+        };
+      },
+      controllerAs: 'panel'
     };
   });
 }) ();
